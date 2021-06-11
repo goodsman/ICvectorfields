3f77ecb6284beaaf67004c50f4d6956420518150
Devin <goodsman@ualberta.ca>
2021-06-11 11:06:19 -0600

    Added test for ReflMat function in utils.R

---
+++ tests/testthat/test-ReflMat.R
@@ -0,0 +1,7 @@
+test_that("test that ReflMat function in utils.R outputs correct matrix", {
+  zmat = matrix(rep(0,9), nrow = 3)
+  zmat[1, 3] = 1
+  zmat[2, 2] = 1
+  zmat[3, 1] = 1
+  expect_equal(ReflMat(3), zmat)
+})
