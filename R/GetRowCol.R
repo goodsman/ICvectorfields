2d5d04f37acaec3101594e52930325f016c51d66
Devin <goodsman@ualberta.ca>
2021-06-11 17:17:41 -0600

    Cleaned up comments in GetRowCol function documentation

--- R/GetRowCol.R
+++ R/GetRowCol.R
@@ -1,19 +1,24 @@
 #' GetRowCol
 #'
-#' Here is a function that will find the row and column associated with a vector index.
+#' Here is a function that will find the row and column indices of a matrix that
+#' are associated with a vector index.
 #'
-#' Often when applying functions like the R function which.max(matrix) to a matrix a vector index
-#' is returned when the coder would prefer to have a row and column indices. This function converts
-#' the vector index to row and column indices.
+#' Often when applying functions like the R function which.max(matrix) to a
+#' matrix a vector index is returned when the coder would prefer to have a row
+#' and column indices. This function converts the vector index to row and column
+#' indices.
 #'
-#' The function assumes that the elements of the matrix are filled by row (byrow = TRUE),
-#' which is the default R matrix behaviour.
+#' The function assumes that the elements of the matrix are filled by row (byrow
+#' = TRUE), which is the default R matrix behaviour.
 #'
 #' @param Index an integer vector index
-#' @param dim1 integer row dimension of the matrix from which the row and column indices are to be extracted
-#' @param dim2 integer column dimension of the matrix from which the row and column indices are to be extracted
+#' @param dim1 integer row dimension of the matrix from which the row and column
+#'   indices are to be extracted
+#' @param dim2 integer column dimension of the matrix from which the row and
+#'   column indices are to be extracted
 #'
-#' @return a numeric vector of length two with two integers indicating row and column respectively
+#' @return a numeric vector of length two with two integers indicating row and
+#'   column respectively
 #' @export
 #'
 #' @examples
