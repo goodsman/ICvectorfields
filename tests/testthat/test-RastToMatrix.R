4af297c260e3bc6b4652306f95c464583840c33e
Devin <goodsman@ualberta.ca>
2021-06-11 14:41:36 -0600

    Added a test for the RastToMatrix function in utils.R

---
+++ tests/testthat/test-RastToMatrix.R
@@ -0,0 +1,5 @@
+test_that("raster to matrix conversion works", {
+  rastz = terra::rast(matrix(1:9, nrow = 3))
+  matz = matrix(1:9, nrow = 3, byrow = TRUE)
+  expect_equal(RastToMatrix(rastz), matz)
+})
