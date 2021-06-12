82352ca6b2316bbe163b23f3c62f1648f69951dd
Devin <goodsman@ualberta.ca>
2021-06-12 10:59:04 -0600

    Corrected false statement in the comment

--- tests/testthat/test-GetRowCol.R
+++ tests/testthat/test-GetRowCol.R
@@ -1,5 +1,5 @@
 test_that("row and column retrival works", {
-  # Assumes that the elements of the matrix are filled by row (byrow = TRUE),
-  # which is the default
+  # Assumes that the elements of the matrix are filled by column (byrow =
+  # FALSE), which is the default
   expect_equal(GetRowCol(6, dim1 = 3, dim2 = 3), c(3, 2))
 })
