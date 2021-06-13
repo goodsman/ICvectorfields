4ab46d3d6958db9c94f4e0d7e993101e04fa7326
Devin <goodsman@ualberta.ca>
2021-06-13 16:52:31 -0600

    Re-styled RastStackData function syntax for consistency and re-flowed the comment.

--- R/RastStackData.R
+++ R/RastStackData.R
@@ -1,27 +1,30 @@
 #' Creating a raster stack from formatted datasets
 #'
 #' This function converts the data that accompany the ICvectorfields R package
-#' to a raster stack. The raster stack is the only accepted data input format for
-#' the following ICvectorfields functions: \code{\link{DispFieldST}}, \code{\link{DispFieldSTbb}},
-#' \code{\link{DispFieldSTall}}, \code{\link{DispFieldSTbball}}.
+#' to a raster stack. The raster stack is the only accepted data input format
+#' for the following ICvectorfields functions: \code{\link{DispFieldST}},
+#' \code{\link{DispFieldSTbb}}, \code{\link{DispFieldSTall}},
+#' \code{\link{DispFieldSTbball}}.
 #'
-#' Once a raster stack has been created, individual layers can be subsetted using
-#' rasterstack[\[index\]], where index is an integer index for the third dimension
-#' of the raster stack. Two individual raster layers are the only accepted data input
-#' format for the following ICvectorfields functions: \code{\link{DispField}},
-#' \code{\link{DispFieldbb}}.
+#' Once a raster stack has been created, individual layers can be subsetted
+#' using rasterstack[\[index\]], where index is an integer index for the third
+#' dimension of the raster stack. Two individual raster layers are the only
+#' accepted data input format for the following ICvectorfields functions:
+#' \code{\link{DispField}}, \code{\link{DispFieldbb}}.
 #'
-#' @param inputdf a data frame object in which the first column is longitude, the
-#'     second column is latitude, and all of the subsequent columns represent a
-#'     measure of population abundance or density at a unique instance of time. Each row of the
-#'     input data frame, therefore, represents a unique spatial location, which should
-#'     be on an evenly spaced grid. Note, however, that not all grid locations need to have
-#'     observations; some grid locations can have values of NA or can be missing entirely.
+#' @param inputdf a data frame object in which the first column is longitude,
+#'   the second column is latitude, and all of the subsequent columns represent
+#'   a measure of population abundance or density at a unique instance of time.
+#'   Each row of the input data frame, therefore, represents a unique spatial
+#'   location, which should be on an evenly spaced grid. Note, however, that not
+#'   all grid locations need to have observations; some grid locations can have
+#'   values of NA or can be missing entirely.
 #'
-#' @return The function returns a raster stack constructed using inputdf. Each layer in the stack corresponds
-#'     to a column of the input dataset (after the first two columns, which are longitude
-#'     and latitude). The extent of all of the rasters in the stack is constructed using
-#'     the minimum and maximum longitudes and latitudes.
+#' @return The function returns a raster stack constructed using inputdf. Each
+#'   layer in the stack corresponds to a column of the input dataset (after the
+#'   first two columns, which are longitude and latitude). The extent of all of
+#'   the rasters in the stack is constructed using the minimum and maximum
+#'   longitudes and latitudes.
 #' @export
 #'
 #' @examples
@@ -38,7 +41,6 @@
 #' terra::plot(zstack[[1]])
 #' terra::plot(zstack[[2]])
 #' terra::plot(zstack[[3]])
-#'
 RastStackData <- function(inputdf) {
 
   # initializing
@@ -47,7 +49,7 @@ RastStackData <- function(inputdf) {
   # creating the stack
   for (i in 3:dim(inputdf)[2]) {
     inputdfrast <- terra::rast(as.matrix(inputdf[, c(1:2, i)]), type = "xyz")
-    terra::ext(inputdfrast) <- c(min(inputdf[, 1]), max(inputdf[,1]), min(inputdf[, 2]), max(inputdf[, 2]))
+    terra::ext(inputdfrast) <- c(min(inputdf[, 1]), max(inputdf[, 1]), min(inputdf[, 2]), max(inputdf[, 2]))
     inputdfstack <- c(inputdfstack, inputdfrast)
   }
 
