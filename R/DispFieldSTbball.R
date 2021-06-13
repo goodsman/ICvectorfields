ecd755819782aac043a213c54b7418aca028ce8f
Devin <goodsman@ualberta.ca>
2021-06-12 23:19:18 -0600

    Added the DsipFieldSTbball function along with detailed function documentation

---
+++ R/DispFieldSTbball.R
@@ -0,0 +1,132 @@
+#' Diplacement fields using bounding box when time lag is unknown
+#'
+#' This is an implementation of a novel algorithm that differs from more
+#' traditional digital image correlation implementations that are applied in the
+#' \code{\link{DispField}} and \code{\link{DispFieldbb}} functions. This version
+#' is similar to the \code{\link{DispFieldSTbb}} function except that it does
+#' not require a specific time lag. Instead the user specifies a maximum time
+#' lag and the function computes displacement vectors using the time lag that
+#' produces the maximum speed (magnitude of displacement divided by time lag).
+#' The function calculates a displacement field representing persistent movement
+#' based on the cross-covariance in a raster stack (in this case a sequential
+#' series of rasters) presumably representing spatial population abundance or
+#' density at more than two different instances of time. If analysis is
+#' restricted to only two time instances, \code{\link{DispFieldbb}} is more
+#' appropriate.
+#'
+#' The DispFieldSTbball function has the same inner workings as the
+#' \code{\link{DispFieldSTbb}} function except that instead of specifying a
+#' specific time lag, the user specifies a maximum time lag. The function then
+#' cycles through all possible lags up to the maximum time lag and choses the
+#' for each location the maximum speed.
+#'
+#' Caution is warranted when defining the bounding box dimensions because the
+#' function can produce erroneous results when the bounding box is too small.
+#'
+#' @param inputstack1 a raster stack with each raster layer representing an
+#'   instance of time. The raster stack should be organized such that the first
+#'   raster in the stack is the first observed spatial dataset and time
+#'   progresses forward with the third dimension index of the raster stack. The
+#'   raster stack should contain only numeric values. Any NA value will be
+#'   converted to a zero
+#' @param lagmax an integer representing the maximum time lag
+#' @param rowmn an integer denoting the minimum row index of the sub-grid
+#' @param rowmx an integer denoting the maximum row index of the sub-grid
+#' @param colmn an integer denoting the minimum column index of the sub-grid
+#' @param colmx an integer denoting the maximum column index of the sub-grid
+#' @param restricted logical (TRUE or FALSE)
+#'
+#' @return A data frame is returned with the following column names: rowcent,
+#'   colcent, frowmin, frowmax, fcolmin, fcolmax, centx, centy, dispx, and
+#'   dispy. The rowcent and colcent column names are the row and column indices
+#'   for the center of each sub-grid; frowmin and frowmax are the sub-grid
+#'   minimum and maximum row indices; fcolmin and fcolmax are the sub-grid
+#'   minimum and maximum column indices; centx and centy are the projected
+#'   coordinates of the centre of the subgrid derived from the raster input
+#'   files; dispx and dispy are the orthoganal velocity vectors in units of
+#'   space per timestep in the horizontal and vertical directions in the same
+#'   spatial units as the projected coordinates of the raster input files.
+#' @export
+#'
+#' @examples
+#' Vec1 <- c(1:5, 0, 0, 0, 0)
+#' Mat1 <- Vec1
+#' for (i in 2:9) Mat1 <- rbind(Mat1, Vec1)
+#' Mat1
+#'
+#' Vec2 <- c(0, 1:5, 0, 0, 0)
+#' Mat2 <- Vec2
+#' for (i in 2:9) Mat2 <- rbind(Mat2, Vec2)
+#' Mat2
+#'
+#' Vec3 <- c(0, 0, 1:5, 0, 0)
+#' Mat3 <- Vec3
+#' for (i in 2:9) Mat3 <- rbind(Mat3, Vec3)
+#' Mat3
+#'
+#' Vec4 <- c(0, 0, 0, 1:5, 0)
+#' Mat4 <- Vec4
+#' for (i in 2:9) Mat4 <- rbind(Mat4, Vec4)
+#' Mat4
+#'
+#' # Note that rasterizing a matrix causes it to be rotated 90 degrees.
+#' # Therefore, any shift in the x direction is in fact now a shift in the y direction
+#' rast1 <- terra::rast(Mat1)
+#' terra::plot(rast1)
+#' rast2 <- terra::rast(Mat2)
+#' terra::plot(rast2)
+#' rast3 <- terra::rast(Mat3)
+#' terra::plot(rast3)
+#' rast4 <- terra::rast(Mat4)
+#' terra::plot(rast4)
+#'
+#' teststack1 <- c(rast1, rast2, rast3, rast4)
+#' (VFdf5 <- DispFieldSTbball(teststack1, lagmax = 2, 1, 9, 1, 9))
+#' # block is moving downward at a speed of 0.1111111 units of space per unit of time
+#' # dispy = -0.1111111
+#'
+DispFieldSTbball <- function(inputstack1, lagmax, rowmn, rowmx, colmn, colmx, restricted = FALSE) {
+  if (lagmax < 1 || lagmax != round(lagmax)) {
+    stop("lagmax must be an integer larger than zero")
+  }
+  if (lagmax >= (dim(inputstack1)[3] - 1)) {
+    stop("lagmax must be at least one smaller than the time demension")
+  }
+  if (lagmax == 1) {
+    stop("no lag > 1; use DispFieldSTbb function")
+  }
+  if (rowmn != round(rowmn)) {
+    stop("rowmn and rowmx must be positive integers")
+  }
+  if (rowmx != round(rowmx)) {
+    stop("rowmn and rowmx must be positive integers")
+  }
+  if (colmn != round(colmn)) {
+    stop("colmn and colmx must be positive integers")
+  }
+  if (colmx != round(colmx)) {
+    stop("colmn and colmx must be positive integers")
+  }
+  if (is.logical(restricted) == FALSE) {
+    stop("restricted must be either TRUE or FALSE")
+  }
+
+  # Initializing
+  Outdf <- DispFieldSTbb(inputstack1, lag1 = 1, rowmn, rowmx, colmn, colmx, restricted = FALSE)
+
+  # cycling through all larger lags
+  for (i in 2:lagmax) {
+    OutdfNew <- DispFieldSTbb(inputstack1, lag1 = i, rowmn, rowmx, colmn, colmx, restricted = FALSE)
+
+    # If the magnitude of displacement in OutdfNew > Outdf, then dispx and
+    # dispy are replaced with the new values
+    MagNew <- sqrt((OutdfNew$dispx^2) + OutdfNew$dispy^2)
+    MagOld <- sqrt((Outdf$dispx^2) + Outdf$dispy^2)
+    MagNew[is.na(MagNew) == TRUE] <- 0
+    MagOld[is.na(MagOld) == TRUE] <- 0
+    Outdf$dispx[MagNew > MagOld] <- OutdfNew$dispx[MagNew > MagOld]
+    Outdf$dispy[MagNew > MagOld] <- OutdfNew$dispy[MagNew > MagOld]
+  }
+
+  return(Outdf)
+}
