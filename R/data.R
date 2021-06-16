b9d55a7c71af6f6a3f71c52bca50b1ee0d328313
Devin <goodsman@ualberta.ca>
2021-06-16 13:17:19 -0600

    Removed broken LaTeX style equation from SimData description and replaced it with a Wikipedia link to the equation

--- R/data.R
+++ R/data.R
@@ -1,29 +1,32 @@
 #' Simulated movement data
 #'
 #' Data based on a partial differential equation were simulated using the
-#' ReacTran R package. The simulation algorith uses a finite differencing scheme
-#' with backwards differencing. The model used for simulation is a reaction
-#' diffusion-advection equation in which the advection term is variable in space
-#' but diffusion and reactions are constant in space:
+#' ReacTran R package (see details).
 #'
-#' \deqn{\frac{\partial(c)}{\partial{t}} = \nabla (D \nabla c) - \nabla (v c) + r}
+#' The simulation algorithm uses a finite differencing scheme with backwards
+#' differencing. The model used for simulation is a reaction diffusion-advection
+#' equation in which the advection term is variable in space but diffusion and
+#' reactions are constant in space see
+#' \href{https://en.wikipedia.org/wiki/Convection%E2%80%93diffusion_equation}{convection-diffusion
+#' equation} for an example.
 #'
-#' The parameters used in the partial differntial equation above are
+#' The parameters used in the general partial differntial equation in the link
+#' above are
 #'
 #' D = 0.01 per squared spatial unit
 #'
-#' r = 0.5 per unit time
+#' R = 0.5 per unit time
 #'
-#' v (advection is variable in space): in the upper left quadrant of the
-#' square domain v = (0.2, 0); in the upper right quadrant v = (0, -0.2);
-#' in the lower right quadrant v = (-0.2, 0); in the lower right quadrant
-#' v = (0, 0.2). Obviously v is discontinous at the quadrant boundaries,
-#' which causes some interesting model behaviour that is limited by
-#' considering only the first six time steps such that the bulk of the
-#' concentration in each quadrant does not cross a quadrant boundary.
+#' v (advection is variable in space): in the upper left quadrant of the square
+#' domain v = (0.2, 0); in the upper right quadrant v = (0, -0.2); in the lower
+#' right quadrant v = (-0.2, 0); in the lower right quadrant v = (0, 0.2).
+#' Obviously v is discontinous at the quadrant boundaries, which causes some
+#' interesting model behaviour that is limited by considering only the first six
+#' time steps such that the bulk of the concentration in each quadrant does not
+#' cross a quadrant boundary.
 #'
-#' The intial condition at time = 0 is a concentration of one unit per
-#' arbitrary unit of volume in the central cell of each quadrant.
+#' The intial condition at time = 0 is a concentration of one unit per arbitrary
+#' unit of volume in the central cell of each quadrant.
 #'
 #' External boundary conditions are zero-gradient (reflecting).
 #'
@@ -32,17 +35,14 @@
 #'
 #' @usage data(SimData)
 #'
-#' @format A data-frame with 40804 rows and 8 columns.
-#' \describe{
-#'   \item{xcoord}{in arbitrary units}
-#'   \item{ycoord}{in arbitrary units}
+#' @format A data-frame with 40804 rows and 8 columns. \describe{
+#'   \item{xcoord}{in arbitrary units} \item{ycoord}{in arbitrary units}
 #'   \item{t1}{concentration in arbitrary units at t = 1}
 #'   \item{t2}{concentration in arbitrary units at t = 2}
 #'   \item{t3}{concentration in arbitrary units at t = 3}
 #'   \item{t4}{concentration in arbitrary units at t = 4}
 #'   \item{t5}{concentration in arbitrary units at t = 5}
-#'   \item{t6}{concentration in arbitrary units at t = 6}
-#' }
+#'   \item{t6}{concentration in arbitrary units at t = 6} }
 #'
 #' @keywords datasets
 
