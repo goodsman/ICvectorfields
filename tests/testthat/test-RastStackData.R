301307647ade98346591059cfccbcd81ff253fc1
Devin <goodsman@ualberta.ca>
2021-06-15 19:03:08 -0600

    Removed unnecessary expect_equal from RastStackData test function

--- tests/testthat/test-RastStackData.R
+++ tests/testthat/test-RastStackData.R
@@ -6,6 +6,5 @@ test_that("RastStackData produces a raster stack with appropriate dimensions", {
 
   zstack <- RastStackData(xyzdf)
   zstack <- terra::rast(zstack)
-  expect_equal(length(zstack), 3)
-  expect_equal(dim(zstack[[1]]), c(3, 3, 1))
+  expect_equal(dim(zstack), c(3, 3, 3))
 })
