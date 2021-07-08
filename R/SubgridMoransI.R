480facec21c7d5ace9b8411a78fc2cd2a39c0007
Devin <goodsman@ualberta.ca>
2021-07-07 18:17:08 -0600

    Added documentation for SubgridMoransI function and NAMESPACE options for Rcpp

--- R/SubgridMoransI.R
+++ R/SubgridMoransI.R
@@ -1,16 +1,39 @@
 #' Compute Moran's I for sub-grids
 #'
+#' A function that facilitates calculation of Moran's I at the sub-grid level.
+#' This may be useful for evaluating spatial autocorrelation as a driver  of
+#' movement speed or direction if used in tandem with \code{\link{DispField}},
+#' \code{\link{DispFieldST}}, or \code{\link{DispFieldSTall}}.
+#'
 #' @importFrom Rcpp sourceCpp
 #'
-#' @param inputrast1
-#' @param factv1
-#' @param facth1
-#' @param rad1
+#' @param inputrast1 a raster as produced by terra::rast
+#' @param factv1 an odd integer for the vertical dimension of sub-grids
+#' @param facth1 an odd integer for the horizontal dimension of sub-grids
+#' @param rad1 an integer indicating the neighbourhood radius in rows/columns.
+#'   Any cell within a distance of rad1 cells of the focal cell is considered to
+#'   be in its neighbourhood.
 #'
-#' @return
+#' @return A data frame is returned with the following column names: rowcent,
+#'   colcent, frowmin, frowmax, fcolmin, fcolmax, and a column for the output
+#'   Moran's I statistic.
 #' @export
 #'
 #' @examples
+#' (TestMat <- matrix(c(1, 0, 1, 0, 1,
+#'                      0, 1, 0, 1, 0,
+#'                      1, 0, 1, 0, 1,
+#'                      0, 1, 0, 1, 0,
+#'                      1, 0, 1, 0, 1),
+#'                     nrow = 5))
+#'
+#' YestRast <- terra::rast(TestMat)
+#' terra::plot(TestRast)
+#'
+#' SubgridMoransI(TestRast, factv1 = 5, facth1 = 5, rad1 = 1)
+#' # using rad = 1 is equivalent to using the rooks neighbourhood
+#' # and so the output should be -1.
+#'
 SubgridMoransI = function(inputrast1, factv1, facth1, rad1 = 1){
   if(factv1/2 == round(factv1/2)) {
     stop("factv1 and facth1 must be odd integers")
