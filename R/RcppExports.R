c77af7ef21c8526ae40bd9fa38dc5c4ba28972c4
Devin <goodsman@ualberta.ca>
2021-07-09 11:46:26 -0600

    Added reference to NA values to function documentation

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -7,7 +7,7 @@
 #' corresponds to the rook's neibhourhood, whereas a radius of 1.5 corresponds to
 #' the queen's neighbourhood.
 #'
-#' @param mat1 a matrix of values
+#' @param mat1 a matrix of values; NA/Inf values must be coded as -999.0 and are ignored
 #' @param r1 the distance (radius), within which nearby cells are considered neighbours
 #'     in units of rows/columns
 #'
