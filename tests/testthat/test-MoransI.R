0f898ba6057a97dd2fda675e1fd92c513400b3e5
Devin <goodsman@ualberta.ca>
2021-07-12 09:15:27 -0600

    Fixed typo

--- tests/testthat/test-MoransI.R
+++ tests/testthat/test-MoransI.R
@@ -6,5 +6,5 @@ test_that("Moran's I works", {
                       1, 0, 1, 0, 1),
                     nrow =5)
   MoransI(TestMat, r1 = 1)
-  expect_equal(round(MoransI(TestMat, 3), r1 = 1), -1)
+  expect_equal(round(MoransI(TestMat, r1 = 1), 3), -1)
 })
