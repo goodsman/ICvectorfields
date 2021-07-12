05c22dab144dd715971297e967714d060e54f1dc
Devin <goodsman@ualberta.ca>
2021-07-12 09:02:59 -0600

    Changed syntax in examples section of MoransI function documentation for consistency

--- src/MoransI.cpp
+++ src/MoransI.cpp
@@ -17,12 +17,12 @@ using namespace Rcpp;
 //' @export
 //'
 //' @examples
-//' (TestMat = matrix(c(1, 0, 1, 0, 1,
-//'                     0, 1, 0, 1, 0,
-//'                     1, 0, 1, 0, 1,
-//'                     0, 1, 0, 1, 0,
-//'                     1, 0, 1, 0, 1),
-//'                   nrow =5))
+//' (TestMat <- matrix(c(1, 0, 1, 0, 1,
+//'                      0, 1, 0, 1, 0,
+//'                      1, 0, 1, 0, 1,
+//'                      0, 1, 0, 1, 0,
+//'                      1, 0, 1, 0, 1),
+//'                    nrow =5))
 //' # the code below should return -1
 //' MoransI(TestMat, r1 = 1)
 //[[Rcpp::export]]
