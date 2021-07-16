c3a911309f63ddb5c3f58a776d8742248919bcd7
Devin <goodsman@ualberta.ca>
2021-07-16 14:42:27 -0600

    Removed unnecessary .unLoad functions

--- R/zzz.R
+++
@@ -1,6 +0,0 @@
-# currently only contains functions to ensure proper clean up of compiled code
-# when package is unloaded.
-
-.onUnload <- function (libpath) {
-  library.dynam.unload("ICvectorfields", libpath)
-}
