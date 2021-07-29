d4c75dd6ff3957acff128f2ee1e19aceb5e5b5fa
Devin <goodsman@ualberta.ca>
2021-07-29 13:44:33 -0600

    Added more patterns to PatternDetect function

--- R/PatternDetect.R
+++ R/PatternDetect.R
@@ -1,10 +1,16 @@
 #' Detect Patterns in Vector Fields
 #'
 #' Detect patterns in vector fields represented on a grid by looking in the
-#' rook's neighbourhood of each grid cell. Two patterns are detected:
+#' rook's neighbourhood of each grid cell. Six patterns are detected:
 #' convergences occur when the vectors in the four adjacent cells in the rook's
 #' neighbourhood point towards the focal cell; divergences occur when the
-#' vectors in the four adjacent cells point away from the focal cell.
+#' vectors in the four adjacent cells point away from the focal cell; vertical
+#' convergences occur when the vectors in the grids above and below the focal
+#' grid point towards it; horzontal convergences occur when the vectors to the
+#' right and left of the focal grid point towards it; vertical divergences occur
+#' when the vectors in the grids above and below the focal grid point away from
+#' it and horizontal divergences occur when the vectors in the grids to the left
+#' and right of the focal grid point away from it.
 #'
 #' @param vfdf A data frame as returned by \code{\link{DispField}},
 #'   \code{\link{DispFieldST}}, or \code{\link{DispFieldSTall}} with at least
@@ -14,6 +20,8 @@
 #'   \code{\link{DispFieldST}}, or \code{\link{DispFieldSTall}}, with two
 #'   additional columns. The first additional column is called Pattern in which
 #'   the patterns around each focal cell are categorized as convergence,
+#'   divergence, 'vertconv' or 'horizconv' for vertical and horizontal
+#'   convergence 'vertdiv' or 'horizdiv' for vertical and horizontal
 #'   divergence, or NA. The second additional column is called PatternCt, which
 #'   contains a one if all four neighbourhood grid locations contain
 #'   displacement estimates and a NA otherwise.
@@ -129,9 +137,29 @@ PatternDetect <- function(vfdf) {
         vfdfout$Pattern[i] <- "convergence"
       }
 
+      # Finding horizontal convergences
+      if (Updy >= 0 & Downdy <= 0 & Leftdx > 0 & Rightdx < 0) {
+        vfdfout$Pattern[i] <- "horizconv"
+      }
+
+      # Finding vertical convergences
+      if (Updy < 0 & Downdy > 0 & Leftdx <= 0 & Rightdx >= 0) {
+        vfdfout$Pattern[i] <- "vertconv"
+      }
+
       # Finding divergences
       if (Updy > 0 & Downdy < 0 & Leftdx < 0 & Rightdx > 0) {
         vfdfout$Pattern[i] <- "divergence"
+      }
+
+      # Finding horizontal divergences
+      if (Updy <= 0 & Downdy >= 0 & Leftdx < 0 & Rightdx > 0) {
+        vfdfout$Pattern[i] <- "horizdiv"
+      }
+
+      # Finding vertical divergences
+      if (Updy > 0 & Downdy < 0 & Leftdx >= 0 & Rightdx <= 0) {
+        vfdfout$Pattern[i] <- "vertdiv"
       }
 
     }
